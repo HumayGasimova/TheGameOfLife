5fe940e4afc8c968c0d202fdf72eb59c93c43d93
Humay <crypto347@gmail.com>
2019-11-22 16:45:03 +0400

    bottom-left corner logic added

--- src/epics/onNextHandlerEpic.js
+++ src/epics/onNextHandlerEpic.js
@@ -37,6 +37,7 @@ export const onNextHandlerEpic = (action$, state$) =>
         mergeMap(action => {
             let arrayOfCells = [...state$.value.theGameOfLife.cells];
             
+                // top-left corner
                 if(arrayOfCells[0].alive === true && arrayOfCells[1].alive === true && arrayOfCells[212].alive === false && arrayOfCells[213].alive === false ||
                     arrayOfCells[0].alive === true && arrayOfCells[212].alive === true && arrayOfCells[1].alive === false && arrayOfCells[213].alive === false ||
                     arrayOfCells[0].alive === true && arrayOfCells[213].alive === true && arrayOfCells[212].alive === false && arrayOfCells[1].alive === false ||
@@ -49,6 +50,7 @@ export const onNextHandlerEpic = (action$, state$) =>
                     arrayOfCells.splice(0, 1, {...arrayOfCells[0], alive: true});
                 }
 
+                // top-right corner
                 if(arrayOfCells[211].alive === true && arrayOfCells[210].alive === true && arrayOfCells[422].alive === false && arrayOfCells[423].alive === false ||
                     arrayOfCells[211].alive === true && arrayOfCells[422].alive === true && arrayOfCells[210].alive === false && arrayOfCells[423].alive === false ||
                     arrayOfCells[211].alive === true && arrayOfCells[423].alive === true && arrayOfCells[210].alive === false && arrayOfCells[422].alive === false ||
@@ -59,6 +61,19 @@ export const onNextHandlerEpic = (action$, state$) =>
                 if(arrayOfCells[211].alive === false && arrayOfCells[210].alive === true && arrayOfCells[422].alive === true && arrayOfCells[423].alive === true
                 ){
                     arrayOfCells.splice(211, 1, {...arrayOfCells[211], alive: true});
+                }
+
+                // bottom-left corner
+                if(arrayOfCells[15052].alive === true && arrayOfCells[14840].alive === true && arrayOfCells[14841].alive === false && arrayOfCells[15053].alive === false ||
+                arrayOfCells[15052].alive === true && arrayOfCells[14841].alive === true && arrayOfCells[14840].alive === false && arrayOfCells[15053].alive === false ||
+                arrayOfCells[15052].alive === true && arrayOfCells[15053].alive === true && arrayOfCells[14840].alive === false && arrayOfCells[14841].alive === false ||
+                arrayOfCells[15052].alive === true && arrayOfCells[14840].alive === false && arrayOfCells[14841].alive === false && arrayOfCells[15053].alive === false
+                ){
+                    arrayOfCells.splice(15052, 1,{...arrayOfCells[15052], alive: false});
+                }
+                if(arrayOfCells[15052].alive === false && arrayOfCells[14840].alive === true && arrayOfCells[14841].alive === true && arrayOfCells[15053].alive === true
+                ){
+                    arrayOfCells.splice(15052, 1, {...arrayOfCells[15052], alive: true});
                 }
 
                 // if(arrayOfCells[212].alive === true ){
