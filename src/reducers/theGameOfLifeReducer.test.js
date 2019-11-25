c3eec63aaa5b42dd3e60ef97c9c1692b57622503
Humay <crypto347@gmail.com>
2019-11-25 17:44:27 +0400

    jest UPDATE_MAP_INTERACTION_CSS reducer added

--- src/reducers/theGameOfLifeReducer.test.js
+++ src/reducers/theGameOfLifeReducer.test.js
@@ -42,7 +42,7 @@ describe('theGameOfLifeReducer', () => {
         expect(reducer(initState, action)).toEqual(state);
     });
 
-    it("should update cells with array passed through the action", () => {
+    it("should update zoom with the value passed through the action", () => {
         const action = { 
             type: actionTypes.UPDATE_ZOOM,
             val: 3
@@ -58,5 +58,94 @@ describe('theGameOfLifeReducer', () => {
         expect(reducer(initState, action)).toEqual(state);
     });
 
-   
+    it("should set scale to 1", () => {
+        const action = { 
+            type: actionTypes.UPDATE_MAP_INTERACTION_CSS,
+            zoom: '1'
+        }
+        const initState = {
+            ...initialState, 
+            scale: 2,
+            translation: { x: 1, y: 1 }
+        }
+        const state = {
+            ...initialState, 
+            scale: 1,
+            translation: { x: 1, y: 1 }
+        }
+        expect(reducer(initState, action)).toEqual(state);
+    });
+
+    it("should set scale to 0.9", () => {
+        const action = { 
+            type: actionTypes.UPDATE_MAP_INTERACTION_CSS,
+            zoom: '2'
+        }
+        const initState = {
+            ...initialState, 
+            scale: 1,
+            translation: { x: 1, y: 1 }
+        }
+        const state = {
+            ...initialState, 
+            scale: 0.9,
+            translation: { x: 1, y: 1 }
+        }
+        expect(reducer(initState, action)).toEqual(state);
+    });
+
+    it("should set scale to 0.8", () => {
+        const action = { 
+            type: actionTypes.UPDATE_MAP_INTERACTION_CSS,
+            zoom: '3'
+        }
+        const initState = {
+            ...initialState, 
+            scale: 2,
+            translation: { x: 1, y: 1 }
+        }
+        const state = {
+            ...initialState, 
+            scale: 0.8,
+            translation: { x: 1, y: 1 }
+        }
+        expect(reducer(initState, action)).toEqual(state);
+    });
+
+    it("should set scale to 0.7", () => {
+        const action = { 
+            type: actionTypes.UPDATE_MAP_INTERACTION_CSS,
+            zoom: '4'
+        }
+        const initState = {
+            ...initialState, 
+            scale: 2,
+            translation: { x: 1, y: 1 }
+        }
+        const state = {
+            ...initialState, 
+            scale: 0.7,
+            translation: { x: 1, y: 1 }
+        }
+        expect(reducer(initState, action)).toEqual(state);
+    });
+
+    it("should set scale to 0.6", () => {
+        const action = { 
+            type: actionTypes.UPDATE_MAP_INTERACTION_CSS,
+            zoom: '5'
+        }
+        const initState = {
+            ...initialState, 
+            scale: 2,
+            translation: { x: 1, y: 1 }
+        }
+        const state = {
+            ...initialState, 
+            scale: 0.6,
+            translation: { x: 1, y: 1 }
+        }
+        expect(reducer(initState, action)).toEqual(state);
+    });
+
 });
